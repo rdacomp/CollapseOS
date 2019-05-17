26e5c1de43445ff4ad09d5b8608fb875181c99f5
Virgil Dupras <hsoft@hardcoded.net>
2019-05-17 13:23:16 -0400

    zasm can now assemble acia!

--- tools/tests/zasm/test6.asm
+++ tools/tests/zasm/test6.asm
@@ -1,3 +1,8 @@
-add	a, b
+.equ	RAMSTART	0x4000
+.equ	ACIA_CTL	0x80	; Control and status. RS off.
+.equ	ACIA_IO		0x81	; Transmit. RS on.
+
 #include "core.asm"
-inc	b
+#include "parse.asm"
+.equ	ACIA_RAMSTART	RAMSTART
+#include "acia.asm"
