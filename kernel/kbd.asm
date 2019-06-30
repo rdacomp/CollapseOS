ad31c7f7f572d3836121ea71cc165feab1e95ed8
Virgil Dupras <hsoft@hardcoded.net>
2019-06-30 13:24:35 -0400

    kbd: make letters lowercase
    We'll make them uppercase when we'll add support for shift.

--- kernel/kbd.asm
+++ kernel/kbd.asm
@@ -72,22 +72,22 @@ kbdGetC:
 	pop	bc
 	jp	unsetZ
 
-; A list of the value associated with the 0x80 possible scan codes of the set
+; A list of the values associated with the 0x80 possible scan codes of the set
 ; 2 of the PS/2 keyboard specs. 0 means no value. That value is a character than
 ; can be read in a GetC routine. No make code in the PS/2 set 2 reaches 0x80.
 kbdScanCodes:
 ; 0x00    1   2   3   4   5   6   7   8   9   a   b   c   d   e   f
 .db   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9,'`',  0
 ; 0x10 9 = TAB
-.db   0,  0,  0,  0,  0,'Q','1',  0,  0,  0,'Z','S','A','W','2',  0
+.db   0,  0,  0,  0,  0,'q','1',  0,  0,  0,'z','s','a','w','2',  0
 ; 0x20 32 = SPACE
-.db   0,'C','X','D','E','4','3',  0,  0, 32,'V','F','T','R','5',  0
+.db   0,'c','x','d','e','4','3',  0,  0, 32,'v','f','t','r','5',  0
 ; 0x30
-.db   0,'N','B','H','G','Y','6',  0,  0,  0,'M','J','U','7','8',  0
+.db   0,'n','b','h','g','y','6',  0,  0,  0,'m','j','u','7','8',  0
 ; 0x40 59 = ;
-.db   0,',','K','I','O','0','9',  0,  0,'.','/','L', 59,'P','-',  0
+.db   0,',','k','i','o','0','9',  0,  0,'.','/','l', 59,'p','-',  0
 ; 0x50 13 = RETURN 39 = '
-.db   0,  0, 39,  0,'[','=',  0,  0,  0,  0,  13,']',  0,'\',  0,  0
+.db   0,  0, 39,  0,'[','=',  0,  0,  0,  0, 13,']',  0,'\',  0,  0
 ; 0x60 8 = BKSP
 .db   0,  0,  0,  0,  0,  0,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0
 ; 0x70 27 = ESC
